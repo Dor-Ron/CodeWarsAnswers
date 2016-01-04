b9ec2f9140dbcc4f993deb7272b120876df4d11a
Dor-Ron <dor.rondel@gmail.com>
2016-01-04 00:32:21 -0500

    more 8kyu kata added

---
+++ 8kyu/how_do_i_compare_numbers.py
@@ -0,0 +1,7 @@
+def what_is(x):
+    if x == 42: #change 'is' to ==
+        return 'everything'
+    elif x == 42 * 42: #change 'is' to ==
+        return 'everything squared'
+    else:
+        return 'nothing'
---
+++ 8kyu/return_to_sanity.py
@@ -0,0 +1,5 @@
+def mystery():
+    results = {"sanity": 'Hello'}  # put sanity in quotations
+    return results  # make sure return statement is properly indented
+
+#Could be simplified to just 'return {"sanity":"Hello"}'
